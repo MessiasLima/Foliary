c69f5f8fa2ba84476de8d97435dbf54b713f53e5
Messias Junior <messiaslima.03@gmail.com>
2026-03-03 18:54:24 +0000

    feat: update analytics injection to use koinInject and provide DebugAnalytics as default

--- foliary/src/commonMain/kotlin/dev/appoutlet/foliary/App.kt
+++ foliary/src/commonMain/kotlin/dev/appoutlet/foliary/App.kt
@@ -1,16 +1,19 @@
 package dev.appoutlet.foliary
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.tooling.preview.Preview
+import dev.appoutlet.foliary.core.analytics.Analytics
 import dev.appoutlet.foliary.core.analytics.LocalAnalytics
 import dev.appoutlet.foliary.core.logging.getKoinLogger
 import dev.appoutlet.foliary.core.ui.theme.FoliaryTheme
 import org.koin.compose.KoinApplication
+import org.koin.compose.koinInject
 import org.koin.core.KoinApplication
 import org.koin.plugin.module.dsl.koinConfiguration
 
-@Preview
+
 @Composable
 fun App() {
     KoinApplication(
@@ -18,7 +21,7 @@ fun App() {
             logger(getKoinLogger())
         }
     ) {
-        val analytics = LocalAnalytics.current
+        val analytics = koinInject<Analytics>()
 
         // Track app launch
         LaunchedEffect(Unit) {
@@ -26,8 +29,10 @@ fun App() {
         }
 
         // Provide analytics to all composables
-        FoliaryTheme {
-            Navigation()
+        CompositionLocalProvider(LocalAnalytics provides analytics) {
+            FoliaryTheme {
+                Navigation()
+            }
         }
     }
 }
