160d401009585a129b3843079710fffa6ca7adf8
Messias Junior <messiaslima.03@gmail.com>
2026-03-04 11:01:17 +0000

    sentry on ios app

--- foliary/src/iosMain/kotlin/Main.kt
+++ foliary/src/iosMain/kotlin/Main.kt
@@ -1,14 +1,5 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import dev.appoutlet.foliary.App
-import dev.appoutlet.foliary.core.logging.initSentry
-import platform.UIKit.UIViewController
-
-// TODO: Add sentry dependency to xcode project
 
 @Suppress("FunctionNaming", "UNUSED")
-fun MainViewController(): UIViewController {
-    initSentry()
-    return ComposeUIViewController {
-        App()
-    }
-}
+fun MainViewController() = ComposeUIViewController { App() }
--- ios/Foliary/iosApp.swift
+++ ios/Foliary/iosApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import FoliaryShared
 
 @main
 struct ComposeApp: App {
+    init() {
+        InitSentryKt.doInitSentry()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView().ignoresSafeArea(.all)
