d023c52d9be6f02badf615361a817421cb877819
Messias Junior <messiaslima.03@gmail.com>
2026-03-15 18:35:32 +0000

    test: fix DeepLinkDispatcherTest.kt

--- foliary/src/commonTest/kotlin/dev/appoutlet/foliary/feature/common/deeplink/DeepLinkDispatcherTest.kt
+++ foliary/src/commonTest/kotlin/dev/appoutlet/foliary/feature/common/deeplink/DeepLinkDispatcherTest.kt
@@ -1,29 +1,53 @@
 package dev.appoutlet.foliary.feature.common.deeplink
 
-import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.flow.first
+import io.kotest.matchers.collections.shouldContainExactly
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class DeepLinkDispatcherTest {
     @Test
     fun `should dispatch deeplink`() = runTest {
         val deeplink = Deeplink.fixture()
+        val received = mutableListOf<Deeplink>()
+
+        val collectorJob = DeepLinkDispatcher.deeplinks.onEach {
+            received.add(it)
+        }.launchIn(this)
 
         DeepLinkDispatcher.dispatch(deeplink)
 
-        DeepLinkDispatcher.deeplinks.first() shouldBe deeplink
+        advanceUntilIdle()
+
+        received.shouldContainExactly(deeplink)
+
+        collectorJob.cancel()
     }
 
     @Test
     fun `should handle multiple deeplinks`() = runTest {
         val deeplink1 = Deeplink.fixture(path = "path1")
         val deeplink2 = Deeplink.fixture(path = "path2")
+        val received = mutableListOf<Deeplink>()
+
+        val collectorJob = DeepLinkDispatcher.deeplinks.onEach {
+            received.add(it)
+        }.launchIn(this)
 
         DeepLinkDispatcher.dispatch(deeplink1)
-        DeepLinkDispatcher.deeplinks.first() shouldBe deeplink1
+
+        advanceUntilIdle()
 
         DeepLinkDispatcher.dispatch(deeplink2)
-        DeepLinkDispatcher.deeplinks.first() shouldBe deeplink2
+
+        advanceUntilIdle()
+
+        received.shouldContainExactly(deeplink1, deeplink2)
+
+        collectorJob.cancel()
     }
 }
