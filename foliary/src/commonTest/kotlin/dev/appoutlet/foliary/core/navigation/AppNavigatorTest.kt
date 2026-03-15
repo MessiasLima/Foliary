8ffd6870580f8ed6083d585f67ce9cf09b79862a
Messias Junior <messiaslima.03@gmail.com>
2026-03-15 12:16:36 +0000

    test: AppNavigatorTest

---
+++ foliary/src/commonTest/kotlin/dev/appoutlet/foliary/core/navigation/AppNavigatorTest.kt
@@ -0,0 +1,61 @@
+package dev.appoutlet.foliary.core.navigation
+
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlin.test.Test
+import kotlinx.serialization.Serializable
+
+class AppNavigatorTest {
+
+    @Test
+    fun `should navigate to new destination when it is different from last`() {
+        val backStack: NavBackStack<NavKey> = NavBackStack(TestNavKey)
+        val navigator = AppNavigator(backStack)
+
+        navigator.navigate(OtherNavKey)
+
+        backStack.last() shouldBe OtherNavKey
+    }
+
+    @Test
+    fun `should not navigate to same destination when it is already at the top`() {
+        val backStack: NavBackStack<NavKey> = NavBackStack(TestNavKey)
+        val navigator = AppNavigator(backStack)
+
+        navigator.navigate(TestNavKey)
+
+        backStack.size shouldBe 1
+    }
+
+    @Test
+    fun `should set root and clear existing stack`() {
+        val backStack: NavBackStack<NavKey> = NavBackStack(TestNavKey, OtherNavKey)
+        val navigator = AppNavigator(backStack)
+
+        navigator.setRoot(ThirdNavKey)
+
+        backStack.size shouldBe 1
+        backStack.last() shouldBe ThirdNavKey
+    }
+
+    @Test
+    fun `should go back to previous destination`() {
+        val backStack: NavBackStack<NavKey> = NavBackStack(TestNavKey, OtherNavKey)
+        val navigator = AppNavigator(backStack)
+
+        navigator.goBack()
+
+        backStack.size shouldBe 1
+        backStack.last() shouldBe TestNavKey
+    }
+}
+
+@Serializable
+private data object TestNavKey : NavKey
+
+@Serializable
+private data object OtherNavKey : NavKey
+
+private data object ThirdNavKey : NavKey
