8081de7b22c38ffad1f75819fb3b071bde84e390
Messias Junior <messiaslima.03@gmail.com>
2026-03-15 12:22:34 +0000

    test: Buttons

---
+++ foliary/src/commonTest/kotlin/dev/appoutlet/foliary/core/ui/component/button/FoliaryOutlinedButtonTest.kt
@@ -0,0 +1,58 @@
+package dev.appoutlet.foliary.core.ui.component.button
+
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.runComposeUiTest
+import io.kotest.matchers.shouldBe
+import kotlin.test.Test
+
+@OptIn(ExperimentalTestApi::class)
+class FoliaryOutlinedButtonTest {
+
+    @Test
+    fun `should show content, be enabled by default and invoke onClick when clicked`() = runComposeUiTest {
+        var clicked = false
+        val testTag = "OutlinedButton"
+        val buttonText = "Click Me"
+
+        setContent {
+            FoliaryOutlinedButton(
+                onClick = { clicked = true },
+                modifier = Modifier.testTag(testTag)
+            ) {
+                Text(buttonText)
+            }
+        }
+
+        onNodeWithText(buttonText).assertExists()
+        onNodeWithTag(testTag).assertIsEnabled().performClick()
+        clicked shouldBe true
+    }
+
+    @Test
+    fun `should be disabled when enabled is false`() = runComposeUiTest {
+        var clicked = false
+        val testTag = "OutlinedButton"
+
+        setContent {
+            FoliaryOutlinedButton(
+                onClick = { clicked = true },
+                enabled = false,
+                modifier = Modifier.testTag(testTag)
+            ) {
+                Text("Disabled")
+            }
+        }
+
+        onNodeWithTag(testTag).assertIsNotEnabled()
+        onNodeWithTag(testTag).performClick()
+        clicked shouldBe false
+    }
+}
