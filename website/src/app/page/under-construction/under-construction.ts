82bdf011b2b18c168550e4563b30c92cb687fd87
Messias Junior <messiaslima.03@gmail.com>
2026-03-16 13:06:12 +0000

    under construction

--- website/src/app/page/under-construction/under-construction.ts
+++ website/src/app/page/under-construction/under-construction.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { FoliaryCard } from '../../component/foliary-card/foliary-card';
 import { FoliaryPrimaryButton } from '../../component/foliary-primary-button/foliary-primary-button';
-import { FoliarySecondaryButton } from '../../component/foliary-secondary-button/foliary-secondary-button';
-import { FoliaryOutlinedButton } from '../../component/foliary-outlined-button/foliary-outlined-button';
 
 @Component({
   selector: 'app-under-construction',
@@ -10,8 +8,6 @@ import { FoliaryOutlinedButton } from '../../component/foliary-outlined-button/f
   imports: [
     FoliaryCard,
     FoliaryPrimaryButton,
-    FoliarySecondaryButton,
-    FoliaryOutlinedButton,
   ],
   templateUrl: './under-construction.html',
   styleUrl: './under-construction.css',
