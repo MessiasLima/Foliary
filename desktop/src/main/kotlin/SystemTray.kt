5e5a93b313ee545f63169255700c495a767c3432
Messias Junior <messiaslima.03@gmail.com>
2026-05-26 18:57:38 +0100

    add todos

--- desktop/src/main/kotlin/SystemTray.kt
+++ desktop/src/main/kotlin/SystemTray.kt
@@ -1,15 +1,19 @@
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.ApplicationScope
 import com.composables.icons.lucide.Leaf
 import com.composables.icons.lucide.Lucide
 import com.kdroid.composetray.tray.api.Tray
+import com.kdroid.composetray.utils.isMenuBarInDarkMode
 import java.awt.Window
 
+// TODO: Create string resources (add rule on readme)
+// TODO: Replace Lucide icon by the application icon
 @Composable
 fun ApplicationScope.FoliaryTray(window: () -> Window) {
     Tray(
         icon = Lucide.Leaf,
-        tint = null,
+        tint = if (isMenuBarInDarkMode()) Color.White else Color.Black,
         tooltip = "Foliary",
         primaryAction = { toggleWindow(window()) }
     ) {
