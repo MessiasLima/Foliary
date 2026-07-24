4a72c5a1eb60ec22e4de4c3aa02621649c3999cb
Messias Junior <messiaslima.03@gmail.com>
2026-07-24 16:10:20 +0100

    import androidx.compose.ui.test.v2.runComposeUiTest

--- foliary/src/commonTest/kotlin/dev/appoutlet/foliary/core/ui/component/layout/LoadingIndicatorTest.kt
+++ foliary/src/commonTest/kotlin/dev/appoutlet/foliary/core/ui/component/layout/LoadingIndicatorTest.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.test.v2.runComposeUiTest
 import kotlin.test.Test
 
 @OptIn(ExperimentalTestApi::class)
--- foliary/src/commonTest/kotlin/dev/appoutlet/foliary/core/ui/component/layout/ScreenTest.kt
+++ foliary/src/commonTest/kotlin/dev/appoutlet/foliary/core/ui/component/layout/ScreenTest.kt
@@ -5,7 +5,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.test.v2.runComposeUiTest
 import dev.appoutlet.foliary.core.analytics.LocalAnalytics
 import dev.appoutlet.foliary.core.analytics.MockAnalytics
 import dev.appoutlet.foliary.core.mvi.ErrorState
