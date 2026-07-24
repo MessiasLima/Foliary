919100cf07c31b4237e867adad22bc9f9ce4a863
Messias Junior <messiaslima.03@gmail.com>
2026-07-24 15:56:28 +0100

    FoliaryBackIconButtonTest.kt

--- foliary/src/commonMain/kotlin/dev/appoutlet/foliary/core/ui/component/button/FoliaryBackIconButton.kt
+++ foliary/src/commonMain/kotlin/dev/appoutlet/foliary/core/ui/component/button/FoliaryBackIconButton.kt
@@ -4,16 +4,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.semantics.semantics
 import foliary.foliary.generated.resources.Res
 import foliary.foliary.generated.resources.back_icon_button_a11y
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun FoliaryBackIconButton(
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    IconButton(onClick = onClick) {
+    IconButton(modifier = modifier, onClick = onClick) {
         Icon(
             imageVector = backIcon,
             contentDescription = stringResource(Res.string.back_icon_button_a11y),
