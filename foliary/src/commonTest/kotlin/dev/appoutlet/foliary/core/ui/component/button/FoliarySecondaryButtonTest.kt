b25fa78146aef8da71230b892e68db7860eddfef
Messias Junior <messiaslima.03@gmail.com>
2026-07-28 09:48:19 +0100

    bump compose test version

--- foliary/src/commonTest/kotlin/dev/appoutlet/foliary/core/ui/component/button/FoliarySecondaryButtonTest.kt
+++ foliary/src/commonTest/kotlin/dev/appoutlet/foliary/core/ui/component/button/FoliarySecondaryButtonTest.kt
@@ -9,7 +9,7 @@ import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.test.v2.runComposeUiTest
 import io.kotest.matchers.shouldBe
 import kotlin.test.Test
 
--- foliary/src/commonTest/kotlin/dev/appoutlet/foliary/feature/signin/composable/AuthenticatedTest.kt
+++ foliary/src/commonTest/kotlin/dev/appoutlet/foliary/feature/signin/composable/AuthenticatedTest.kt
@@ -3,7 +3,7 @@ package dev.appoutlet.foliary.feature.signin.composable
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.test.v2.runComposeUiTest
 import dev.appoutlet.foliary.feature.signin.SignInViewData
 import kotlin.test.Test
 
